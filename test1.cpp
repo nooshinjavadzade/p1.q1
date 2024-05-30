8327b43a847ceee9e367f75faa60ab4028e35c23
nooshinjavadzade <javadzadenooshin@gmail.com>
2024-05-30 22:58:42 +0330

    sort alephba

--- test1.cpp
+++ test1.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <string>
-#include <string.h>
-#include <random>
 using namespace std;
-void sort(string alephba[25][10],string input[],int size, int aski1, int aski2){
-   int j =0;
-   for (int i = 0; i < size; i++)
-   {
-      int k=0;
-      while (input[i][k]==' ')
-      {
-         k++;
-      }
-      if(input[i][k] == aski1 || input[i][k]==aski2){
-         alephba[aski1-65][j];
-         j++;
-      }
-   }
+
+void sort(string alephba[200][10], string input[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (static_cast<int>(input[i][0])== '-')
+        {
+            int ascii = static_cast<int>(input[i][1]); // Convert first character to ASCII code
+            alephba[ascii][0] = input[i];
+        }
+        else
+        {
+            int ascii = static_cast<int>(input[i][0]); // Convert first character to ASCII code
+            alephba[ascii][0] = input[i];
+        }
+        
+        
+       
+    }
 }
-int main(){
-   string x[25][10];
-   x[0][0]="hi babe";
-   if (x[0][0].empty())
-   {
-      cout << 1;
-   }
-   else
-   {
-      cout << 0;
-   }
-   
-   
-   /*for (int i = 0; i < 4; i++)
-   {
-      
-   }*/
-   
-   
-   return 0;
+
+int main() {
+    string x[200][10];
+    string in[10];
+    for (int i = 0; i < 4; i++) {
+        cin >> in[i];
+    }
+
+    sort(x, in, 4);
+
+    for (int i = 0; i < 200; i++) {
+        if (!x[i][0].empty()) {
+            cout << x[i][0] << endl;
+        }
+    }
+
+    return 0;
 }
