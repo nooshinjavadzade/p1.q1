5634ce81c1bfc82bcb11a49943283dc4715ff722
nooshinjavadzade <javadzadenooshin@gmail.com>
2024-06-01 18:47:46 +0330

    lastest update

--- master.cpp
+++ master.cpp
@@ -3,7 +3,30 @@
 #include <string.h>
 #include <random>
 using namespace std;
+bool x(string input[100], int i, int k) {
+    while (input[i][k] == ' ' || input[i][k] == '_' || input[i][k] == '-' || input[i][k] == '!' || input[i][k] == '@' || input[i][k] == ')' || input[i][k] == '(' || input[i][k] == '%' || (input[i][k] < 58 && input[i][k] > 47)) {
+        return false;
+    }
+    return true;
+}
 
+void sort(string arr[200][20], string input[100], int size) {
+    for (int i = 0; i < size; i++) {
+        int k = 0;
+        while (!x(input, i, k)) {
+            k++;
+        }
+
+        char firstChar = tolower(input[i][k]);
+        int index2 = 0;
+        for (int j = 97; j < 123; j++) {
+            if (j == firstChar) {
+                arr[j][index2] = input[i];
+                index2++;
+            }
+        }
+    }
+}
 
 int main(){
    int size;
@@ -13,7 +36,6 @@ int main(){
    {
       getline(cin ,input[i]);
    }
-
-   
+   string alephba[10][10];
    return 0;
 }
